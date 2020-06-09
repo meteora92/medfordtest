95442218f82108e57d9a1e172e2eac764af75315
meteora92 <66653614+meteora92@users.noreply.github.com>
2020-06-09 04:29:20 +0300

    Add files via upload

--- Bot1.py
+++ Bot1.py
@@ -1,8 +1,8 @@
 import telebot
-from telebot import apihelper
+#from telebot import apihelper
 
 bot = telebot.TeleBot('1233009953:AAFQwx_3GRp53bFsoCVmib4yqYKdeyLEPwU')
-apihelper.proxy = ("https" 'socks5://45.225.88.147:999')
+#apihelper.proxy = ("https" 'socks5://45.225.88.147:999')
 
 @bot.message_handler(commands=['start'])
 def lalala(message):
